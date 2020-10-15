e76d28d1b3876bea41ac9ec3c079103407de7789
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-10-15 16:00:40 +0530

    some changes

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
+//Defining routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
@@ -19,12 +25,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-//Defining routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
